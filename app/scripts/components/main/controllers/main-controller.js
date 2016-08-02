ffda619abf1ede05faaa51b9c623ae4fe3874106
Patrick Opie <opiepj@plu.edu>
2016-08-02 10:03:24 -0700

    Todo List

--- app/scripts/components/main/controllers/main-controller.js
+++ app/scripts/components/main/controllers/main-controller.js
@@ -7,10 +7,55 @@
  * # Controller for the about page
  */
 angular.module('myAppMain')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+
+    $scope.todoList = ['Clean', 'Bake'];
+
+
+    $scope.addTodo = function (todoItem) {
+      $scope.todoList.push(todoItem);
+    };
+
+    $scope.removeTodo = function (item) {
+      var index;
+      if (typeof item === 'string') {
+        index = $scope.todoList.indexOf(item);
+      } else if (typeof item === 'number') {
+        index = item;
+      } else {
+        return null;
+      }
+      if (index > -1) {
+        $scope.todoList.splice(index, 1);
+      }
+    };
+
+
+
+    /*
+    $scope.name = 'Success!';
+    $scope.alert = function () {
+      alert('Hello');
+    };
+
+    $scope.random = function () {
+      var array = [
+        'apples',
+        'pears',
+        'oranges',
+        'peaches',
+        'pugs',
+        'carrots',
+        'chocolate'
+      ];
+
+      var index = Math.floor(Math.random() * 6);
+
+      return array[index];
+    };
+    */
+
+
+
+
   });
